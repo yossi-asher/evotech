cb8e97c8ac161f3bf5afe25faec1c94e0b4862de
Yossi Asher <yossiasher@microsoft.com>
2022-11-13 20:29:34 +0200

    fix

--- simple_demo.py
+++ simple_demo.py
@@ -1,5 +1,3 @@
-# import os
-# os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python' 
 
 import pkg_resources
 protobuf_version = [i.version for i in pkg_resources.working_set if i.key == 'protobuf'][0]
